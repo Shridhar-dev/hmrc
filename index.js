d6ec518eb9fc50d4be9d93ed57b774656812d801
Shridhar <shridharkamat10@gmail.com>
2024-02-28 00:31:00 +0530

    feat: first one

---
+++ index.js
@@ -0,0 +1,36 @@
+const { exec } = require('child_process');
+const fs = require('fs')
+const { program } = require('commander');
+
+program
+  .option('-p, --path <string>');
+
+program.parse();
+
+const options = program.opts();
+const path = options.path ? options.path : "";
+const name = path.split("/")[path.split("/").length-1];
+
+
+console.log(`Running ${name}...`);
+
+function runFile(path){
+    console.clear()
+    const compiler = path[path.length-1] === "c" ? "gcc" : "g++";
+    exec(`${compiler} ${path} -o a`, (error)=>{
+        if(error){
+            console.error(error)
+        }
+        else{
+            const terminal = exec("a.exe");
+            terminal.stdout.on('data', async(data) => {
+                console.log(data)
+            });
+        }
+    });
+}
+
+runFile(path);
+fs.watchFile(path,{ interval: 0 },()=>{
+    runFile(path);
+})
